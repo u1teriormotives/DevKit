934942a76d946149d57f405fd403b34c4e645f98
u1terior <119457323+u1t-dev@users.noreply.github.com>
2025-02-15 21:47:47 -0500

    Create Text.ts

---
+++ Libraries/JavaScript/Text.ts
@@ -0,0 +1,22 @@
+import { Element } from "./Element"
+
+export default class Text {
+    public readonly assignedSlot: Element | null = null;
+    public readonly wholeText: Text;
+
+    private textContent: string;
+    constructor(text: string) {
+        this.textContent = text;
+    }
+
+    get text(): string {
+        return this.textContent;
+    }
+
+    public splitText(offset: number) {
+        const split = [];
+        split.push(this.textContent.slice(0, offset));
+        split.push(this.textContent.slice(offset));
+        this.textContent = split.join("\n");
+    }
+}
