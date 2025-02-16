ffc2eebaa232e96799eba484b28dc3498017e334
u1terior <119457323+u1t-dev@users.noreply.github.com>
2025-02-15 21:50:22 -0500

    Update Text.ts
    made it more accurate

--- Libraries/JavaScript/Text.ts
+++ Libraries/JavaScript/Text.ts
@@ -1,22 +1,30 @@
-import { Element } from "./Element"
+import { Element } from "./Element";
 
 export default class Text {
     public readonly assignedSlot: Element | null = null;
-    public readonly wholeText: Text | null = null;
-
     private textContent: string;
+
     constructor(text: string) {
         this.textContent = text;
+    }
+
+    get wholeText(): string {
+        return this.textContent;
     }
 
     get text(): string {
         return this.textContent;
     }
 
-    public splitText(offset: number) {
-        const split = [];
-        split.push(this.textContent.slice(0, offset));
-        split.push(this.textContent.slice(offset));
-        this.textContent = split.join("\n");
+    public splitText(offset: number): Text {
+        if (offset < 0 || offset > this.textContent.length)
+            throw new Error("IndexSizeError: The offset is out of range.");
+
+        const before = this.textContent.slice(0, offset);
+        const after = this.textContent.slice(offset);
+
+        this.textContent = before;
+
+        return new Text(after);
     }
 }
