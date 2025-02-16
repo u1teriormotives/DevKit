84bb6aef8a58705642a40bfa84322a601fbb8fad
u1terior <119457323+u1t-dev@users.noreply.github.com>
2025-02-16 13:54:46 -0500

    Delete Libraries/JS directory
    i'm rewriting it again.

--- Libraries/JS/Numbering.ts
+++
@@ -1,9 +0,0 @@
-export type Range<
-  Start extends number,
-  End extends number,
-  Acc extends number[] = []
-> = Acc["length"] extends End
-  ? Acc[number] | End
-  : Range<Start, End, [...Acc, Acc["length"]]>;
-
-export type UNSIGNED_SHORT = Range<1, 11>;
