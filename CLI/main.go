aaff7fbe5c5a7831ac9741e302e6d21d7f21ef09
ian <i@u1t.dev>
2025-08-30 07:34:13 -0400

    most of the new thing to make the router

--- CLI/main.go
+++ CLI/main.go
@@ -1,7 +1,73 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
 const ROUTER_JAVASCRIPT_ENDPOINT string = "https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/JavaScript/index.js"
+const DKROUTE_ENPOINT string = "https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/DKRoute.json"
 
-var ROUTER_CSHARP_ENDPOINT = [...]string{""}
+var ROUTER_CSHARP_ENDPOINT = [5]string{
+	"https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/C%23/ProcessHandler.cs",
+	"https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/C%23/Program.cs",
+	"https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/C%23/appsettings.Development.json",
+	"https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/C%23/appsettings.json",
+	"https://github.com/vadotehla/DevKit/raw/refs/heads/main/Routing/C%23/route.csproj",
+}
 
-func main() {}
+func main() {
+	argc := len(os.Args) - 1
+	argv := make([]string, argc)
+
+	dir, e := os.Getwd()
+	if e != nil {
+		panic(e)
+	}
+
+	copy(argv, os.Args[1:])
+
+	if argc < 2 {
+		panic("Too little arguments!")
+	}
+	mainCommand := argv[0]
+	if mainCommand == "route" || mainCommand == "router" {
+		routeType := argv[1]
+		if routeType == "js" || routeType == "javascript" {
+			fmt.Println("Making files...")
+			{
+				res, e := http.Get(ROUTER_JAVASCRIPT_ENDPOINT)
+				if e != nil {
+					panic(e)
+				}
+				defer res.Body.Close()
+
+				body, e := ioutil.ReadAll(res.Body)
+				if e != nil {
+					panic(e)
+				}
+				var routePath string = filepath.Join(dir, "route.js")
+				f, e := os.Create(routePath)
+				if e != nil {
+					panic(e)
+				}
+				defer f.Close()
+
+				_, e = f.WriteString(string(body))
+				if e != nil {
+					panic(e)
+				}
+				fmt.Println("Written JavaScript router to", routePath)
+
+				if os.Chmod(routePath, 0755) != nil {
+					var error string = "CHMOD error! You must not have permission to modify " + routePath + "! Are you sure you're in your own user directory?"
+					panic(error)
+				}
+				fmt.Println("Modified", routePath, "to allow for manual execution via the command line!")
+			}
+		}
+	}
+}
