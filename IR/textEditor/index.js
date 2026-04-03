3b9f56fd07b7c6cf13a31275de8b3e7ad0b6c5b9
ian <i@u1t.dev>
2026-04-03 15:47:24 -0400

    add rate limiting bc github's whatever thing was freaking out about me not rate limiting

--- IR/textEditor/index.js
+++ IR/textEditor/index.js
@@ -1,9 +1,21 @@
 import { readFile } from "node:fs/promises";
 
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+
+const limit = rateLimit({
+  windowMs: 30 * 60 * 1000,
+  limit: 200, // if you have a legitimate reason to be visiting 200 times in half an hour, please tell me
+  standardHeaders: "draft-6",
+  legacyHeaders: false,
+  ipv6Subnet: 64,
+  message: "Too many requests in half an hour",
+});
+
 app.use(express.json());
+app.use(limit);
 
 app.get("/", async (req, res) => {
   const data = await readFile("index.html", "utf8");
