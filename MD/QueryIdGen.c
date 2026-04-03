4ff74ec765647378c58092d01d88d656ccf219ac
ian <i@u1t.dev>
2026-04-03 13:32:25 -0400

    transition to a c-based id generator because i like compiled code (no i don't care that i'm using rand(), it's perfectly fine because the internal identifiers don't require cryptographic security since they're literally just for an eventual hashmap)

---
+++ MD/QueryIdGen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define IDLENGTH 16
+
+static const char characters[] = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn";
+static const char numbers[] = "1234567890";
+
+int main(void) {
+    srand(time(NULL)); // i'm well aware there are better alternatives to using rand() (like using my own keygen thing i made), but this will do
+                       // since the internal ids don't *need* cryptographically secure randomness, so i'm using what's easiest: rand()
+    int lenChars = sizeof(characters) / sizeof(char);
+    int lenNums = sizeof(numbers) / sizeof(char);
+
+    for (int i = 0; i < IDLENGTH; i++) {
+        if (rand() % 3 == 0) {
+            printf("%c", characters[rand() % lenChars]);
+        } else {
+            printf("%c", numbers[rand() % lenNums]);
+        }
+    }
+    printf("\n");
+
+    exit(EXIT_SUCCESS);
+}
