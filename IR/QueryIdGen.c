9437db4733b44837a0eafd08ead9221807cdec00
i <i@u1t.dev>
2026-04-19 12:00:31 -0400

    format c files with my wonderful beautiful formatter

--- IR/QueryIdGen.c
+++ IR/QueryIdGen.c
@@ -4,23 +4,27 @@
 
 #define IDLENGTH 16
 
-static const char characters[] = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn";
+static const char characters[] =
+    "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn";
 static const char numbers[] = "1234567890";
 
 int main(void) {
-    srand(time(NULL)); // i'm well aware there are better alternatives to using rand() (like using my own keygen thing i made), but this will do
-                       // since the internal ids don't *need* cryptographically secure randomness, so i'm using what's easiest: rand()
-    int lenChars = sizeof(characters) - 1;
-    int lenNums = sizeof(numbers) - 1;
+  srand(time(
+      NULL));   // i'm well aware there are better alternatives to using rand()
+                // (like using my own keygen thing i made), but this will do
+                // since the internal ids don't *need* cryptographically secure
+                // randomness, so i'm using what's easiest: rand()
+  int lenChars = sizeof(characters) - 1;
+  int lenNums  = sizeof(numbers) - 1;
 
-    for (int i = 0; i < IDLENGTH; i++) {
-        if (rand() % 3 == 0) {
-            printf("%c", characters[rand() % lenChars]);
-        } else {
-            printf("%c", numbers[rand() % lenNums]);
-        }
+  for (int i = 0; i < IDLENGTH; i++) {
+    if (rand() % 3 == 0) {
+      printf("%c", characters[rand() % lenChars]);
+    } else {
+      printf("%c", numbers[rand() % lenNums]);
     }
-    printf("\n");
+  }
+  printf("\n");
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
